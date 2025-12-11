9cbe4d8c0d5c4c3fd74e92fb74080f71838eb13a
alexbabkin <lutron1983@gmail.com>
2025-12-12 01:02:01 +0300

    add new tests

--- conftest.py
+++ conftest.py
@@ -5,7 +5,7 @@ from playwright.sync_api import sync_playwright
 def browser():
     """Create a browser instance for all tests"""
     with sync_playwright() as p:
-        browser = p.chromium.launch(headless=False)
+        browser = p.chromium.launch(headless=True)
         yield browser
         browser.close()
 
